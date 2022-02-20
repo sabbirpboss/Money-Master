865c8d9bc06f45d184dcbf95c9feb601a377e8d8
sabbirpboss <sabbir.ah799@gmail.com>
2022-02-21 01:22:42 +0600

    calculation part and saving part and remaining calculation completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//calculation function
 document.getElementById('calculate-button').addEventListener('click', function(){
 
     //get expenses value 
@@ -9,21 +10,23 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const clothCost = clothCostField.value;
 
     //error handling
-  /* 
+/*   
     const foodCost = parseFloat(foodCostField);
     const rentCost = parseFloat(rentCostField);
     const clothCost = parseFloat(clothCostField);
-if (typeof foodCost != "number" || typeof rentCost != "number" || typeof clothCost != "number") {
+if (typeof foodCost == "string" || typeof rentCost == "string" || typeof clothCost == "string") {
     console.log('please insert a number');
 }
-else if (foodCost <= 0 || rentCost <= 0 || clothCost <= 0) {
-    console.log('Please inset a positive value');
-}
+// else if (foodCost <= 0 || rentCost <= 0 || clothCost <= 0) {
+//     console.log('Please inset a positive value');
+// }
 else{
     const totalExpenses = foodCost + rentCost + clothCost;
-    console.log(calculateExpenses);
+    console.log(totalExpenses);
 }
+
  */
+
 //summation total expenses
 const totalExpensesCalculation = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothCost);
 
@@ -41,4 +44,27 @@ const presentBalance = myIncome - totalExpensesCalculation;
 const balance = document.getElementById('balance');
 const balanceText = balance.innerText;
 balance.innerText = presentBalance;
+
+});
+
+//saving function
+document.getElementById('saving-button').addEventListener('click', function () {
+
+    const myIncome = document.getElementById('my-income').value;
+    
+    //get saving value
+    const percentField = document.getElementById('percent-field');
+    const savingValue = percentField.value;
+    //saving calculation
+    const savingCalculation = myIncome * savingValue / 100;
+    //show final saving amount
+    const savingAmount = document.getElementById('saving-amount').innerText = savingCalculation;
+
+    //remaining balance
+    
+    const presentBalance = document.getElementById('balance').innerText;
+   //calculation remaining amount 
+    const remainingAmount = presentBalance - savingAmount;
+// show remaining amount
+const remainingBalance = document.getElementById('remaining-balance').innerText = remainingAmount;
 })
